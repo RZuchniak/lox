5a5c31b8b0650dae1ab5458b601273bf998c1425
RZuchniak <robert.zuchniak@hotmail.com>
2025-06-03 21:38:59 -0400

    feat: finish control flow

--- Parser.java
+++ Parser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Parser {
@@ -31,14 +32,59 @@ class Parser {
 	}
 
 	private Stmt statement() {
+		if (match(TokenType.FOR))
+			return forStatement();
 		if (match(TokenType.IF))
 			return ifStatement();
 		if (match(TokenType.PRINT))
 			return printStatement();
+		if (match(TokenType.WHILE))
+			return whileStatement();
 		if (match(TokenType.LEFT_SQUIGLY))
 			return new Stmt.Block(block());
 
 		return expressionStatement();
+	}
+
+	private Stmt forStatement() {
+		consume(TokenType.LEFT_BRACKET, "Expect '(' after 'for'");
+
+		Stmt initializer;
+		if (match(TokenType.SEMICOLON)) {
+			initializer = null;
+		} else if (match(TokenType.VAR)) {
+			initializer = varDeclaration();
+		} else {
+			initializer = expressionStatement();
+		}
+
+		Expr condition = null;
+		if (!check(TokenType.SEMICOLON)) {
+			condition = expression();
+		}
+		consume(TokenType.SEMICOLON, "Expect ';' after loop condition");
+
+		Expr increment = null;
+		if (!check(TokenType.RIGHT_BRACKET)) {
+			increment = expression();
+		}
+		consume(TokenType.RIGHT_BRACKET, "Expect ')' after for clauses");
+		Stmt body = statement();
+
+		if (increment != null) {
+			body = new Stmt.Block(
+				Arrays.asList(body, new Stmt.Expression(increment)));
+		}
+
+		if (condition == null)
+			condition = new Expr.Literal(true);
+		body = new Stmt.While(condition, body);
+
+		if (initializer != null) {
+			body = new Stmt.Block(Arrays.asList(initializer, body));
+		}
+
+		return body;
 	}
 
 	private Stmt ifStatement() {
@@ -48,7 +94,7 @@ class Parser {
 
 		Stmt thenBranch = statement();
 		Stmt elseBranch = null;
-		if (match(Tokentype.ELSE)) {
+		if (match(TokenType.ELSE)) {
 			elseBranch = statement();
 		}
 		return new Stmt.If(condition, thenBranch, elseBranch);
@@ -69,6 +115,14 @@ class Parser {
 		}
 		consume(TokenType.SEMICOLON, "Expect ';' after variable declaration");
 		return new Stmt.Var(name, initializer);
+	}
+
+	private Stmt whileStatement() {
+		consume(TokenType.LEFT_BRACKET, "Expect '(' after 'while'");
+		Expr condition = expression();
+		consume(TokenType.RIGHT_BRACKET, "Expect ')' after condition");
+		Stmt body = statement();
+		return new Stmt.While(condition, body);
 	}
 
 	private Stmt expressionStatement() {
--- Stmt.java
+++ Stmt.java
@@ -1,79 +1,88 @@
 import java.util.List;
 
 abstract class Stmt {
-  interface Visitor<R> {
-    R visitBlockStmt(Block stmt);
-    R visitExpressionStmt(Expression stmt);
-    R visitIfStmt(If stmt);
-    R visitPrintStmt(Print stmt);
-    R visitVarStmt(Var stmt);
-  }
- static class Block extends Stmt {
-   Block(List<Stmt> statements) {
-    this.statements = statements;
-    }
+	interface Visitor<R> {
+		R visitBlockStmt(Block stmt);
+		R visitExpressionStmt(Expression stmt);
+		R visitIfStmt(If stmt);
+		R visitPrintStmt(Print stmt);
+		R visitVarStmt(Var stmt);
+		R visitWhileStmt(While stmt);
+	}
+	static class Block extends Stmt {
+		Block(List<Stmt> statements) { this.statements = statements; }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitBlockStmt(this);
-    }
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitBlockStmt(this);
+		}
 
-    final List<Stmt> statements;
-  }
- static class Expression extends Stmt {
-   Expression(Expr expression) {
-    this.expression = expression;
-    }
+		final List<Stmt> statements;
+	}
+	static class Expression extends Stmt {
+		Expression(Expr expression) { this.expression = expression; }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitExpressionStmt(this);
-    }
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitExpressionStmt(this);
+		}
 
-    final Expr expression;
-  }
- static class If extends Stmt {
-   If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
-    this.condition = condition;
-    this.thenBranch = thenBranch;
-    this.elseBranch = elseBranch;
-    }
+		final Expr expression;
+	}
+	static class If extends Stmt {
+		If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+			this.condition = condition;
+			this.thenBranch = thenBranch;
+			this.elseBranch = elseBranch;
+		}
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitIfStmt(this);
-    }
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitIfStmt(this);
+		}
 
-    final Expr condition;
-    final Stmt thenBranch;
-    final Stmt elseBranch;
-  }
- static class Print extends Stmt {
-   Print(Expr expression) {
-    this.expression = expression;
-    }
+		final Expr condition;
+		final Stmt thenBranch;
+		final Stmt elseBranch;
+	}
+	static class Print extends Stmt {
+		Print(Expr expression) { this.expression = expression; }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitPrintStmt(this);
-    }
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitPrintStmt(this);
+		}
 
-    final Expr expression;
-  }
- static class Var extends Stmt {
-   Var(Token name, Expr initializer) {
-    this.name = name;
-    this.initializer = initializer;
-    }
+		final Expr expression;
+	}
+	static class Var extends Stmt {
+		Var(Token name, Expr initializer) {
+			this.name = name;
+			this.initializer = initializer;
+		}
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-        return visitor.visitVarStmt(this);
-    }
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitVarStmt(this);
+		}
 
-    final Token name;
-    final Expr initializer;
-  }
+		final Token name;
+		final Expr initializer;
+	}
+	static class While extends Stmt {
+		While(Expr condition, Stmt body) {
+			this.condition = condition;
+			this.body = body;
+		}
 
-  abstract <R> R accept(Visitor<R> visitor);
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitWhileStmt(this);
+		}
+
+		final Expr condition;
+		final Stmt body;
+	}
+
+	abstract <R> R accept(Visitor<R> visitor);
 }
