92e1cc6750e9f14a5d5f9093b49e443b7737fb38
RZuchniak <robert.zuchniak@hotmail.com>
2025-08-14 11:11:25 -0400

    feat: finish resolver implementation

--- Environment.java
+++ Environment.java
@@ -2,12 +2,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+
 	final Environment enclosing;
 	private final Map<String, Object> values = new HashMap<>();
 
-	Environment() { enclosing = null; }
+	Environment() {
+		enclosing = null;
+	}
 
-	Environment(Environment enclosing) { this.enclosing = enclosing; }
+	Environment(Environment enclosing) {
+		this.enclosing = enclosing;
+	}
 
 	Object get(Token name) {
 		if (values.containsKey(name.lexeme)) {
@@ -18,8 +23,10 @@ public class Environment {
 			return enclosing.get(name);
 		}
 
-		throw new RuntimeError(name,
-							   "undefined variable '" + name.lexeme + "'");
+		throw new RuntimeError(
+			name,
+			"undefined variable '" + name.lexeme + "'"
+		);
 	}
 
 	void assign(Token name, Object value) {
@@ -33,9 +40,29 @@ public class Environment {
 			return;
 		}
 
-		throw new RuntimeError(name,
-							   "undefined variable '" + name.lexeme + "'");
+		throw new RuntimeError(
+			name,
+			"undefined variable '" + name.lexeme + "'"
+		);
 	}
 
-	void define(String name, Object value) { values.put(name, value); }
+	void define(String name, Object value) {
+		values.put(name, value);
+	}
+
+	Environment ancestor(int distance) {
+		Environment environment = this;
+		for (int i = 0; i < distance; i++) {
+			environment = environment.enclosing;
+		}
+		return environment;
+	}
+
+	Object getAt(int distance, String name) {
+		return ancestor(distance).values.get(name);
+	}
+
+	void assignAt(int distance, Token name, Object value) {
+		ancestor(distance).values.put(name.lexeme, value);
+	}
 }
--- Lox.java
+++ Lox.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+
 	private static final Interpreter interpreter = new Interpreter();
 	static boolean hadError = false;
 	static boolean hadRuntimeError = false;
@@ -26,10 +27,8 @@ public class Lox {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
 
-		if (hadError)
-			System.exit(65);
-		if (hadRuntimeError)
-			System.exit(70);
+		if (hadError) System.exit(65);
+		if (hadRuntimeError) System.exit(70);
 	}
 
 	private static void runPrompt() throws IOException {
@@ -39,8 +38,7 @@ public class Lox {
 		for (;;) {
 			System.out.print("> ");
 			String line = reader.readLine();
-			if (line == null)
-				break;
+			if (line == null) break;
 			run(line);
 			hadError = false;
 		}
@@ -52,23 +50,31 @@ public class Lox {
 		Parser parser = new Parser(tokens);
 		List<Stmt> statements = parser.parse();
 
-		if (hadError)
-			return;
+		if (hadError) return;
+
+		Resolver resolver = new Resolver(interpreter);
+		resolver.resolve(statements);
+
+		if (hadError) return;
 
 		interpreter.interpret(statements);
 	}
 
-	static void error(int line, String message) { report(line, "", message); }
+	static void error(int line, String message) {
+		report(line, "", message);
+	}
 
 	static void runtimeError(RuntimeError error) {
-		System.err.println(error.getMessage() + "\n[line " + error.token.line +
-						   "]");
+		System.err.println(
+			error.getMessage() + "\n[line " + error.token.line + "]"
+		);
 		hadRuntimeError = true;
 	}
 
 	private static void report(int line, String where, String message) {
-		System.err.println("[line " + line + "] Error" + where + ": " +
-						   message);
+		System.err.println(
+			"[line " + line + "] Error" + where + ": " + message
+		);
 		hadError = true;
 	}
 
